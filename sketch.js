426daba7424b11d368be5cd80d61242dc911a46b
AleMunoz <alemunoz@uol.com.br>
2022-05-23 19:56:52 -0300

    O que fiz?
    Adicionei fundo ao jogo e criamos o primeiro sprite.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,21 @@
+var fundo;
+var homem;
+
+
 function preload(){
+fundo = createImg("assets/tela de fundo.gif");
+
 
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  homem = createSprite(20,height -50,30,100);
 }
 
 function draw() {
   background(0); 
-
+  fundo.size(windowWidth, windowHeight);
+ drawSprites();
 }
 
