6a5497d62aa15e53727a965858ca8a509d2b7953
teachermari009 <Marips0202@gmail.com>
2022-05-30 19:36:32 -0300

    Adicionando bibliotecas

--- sketch.js
+++ sketch.js
@@ -1,21 +1,22 @@
 var fundo;
-var homem;
+var homem, homemImage;
 
 
 function preload(){
-fundo = createImg("assets/tela de fundo.gif");
-
+  fundo = loadImage("assets/tela de fundo.gif");
+  homemImage = loadImage("assets/shooter_2.png");
 
 }
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
-  homem = createSprite(20,height -50,30,100);
+  homem = createSprite(50,height - 100,30,100);
+  homem.addImage("normal", homemImage);
+  homem.scale = 0.5;
 }
 
 function draw() {
-  background(0); 
-  fundo.size(windowWidth, windowHeight);
- drawSprites();
+  image(fundo,0,0,width,height);
+  drawSprites();
 }
 
